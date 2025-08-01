2fa0309f358595c7b50743264d619ab8c0aba79f
matheus130607 <matheusmalaman2014@gmail.com>
2025-08-01 16:47:18 -0300

    Atualizado

---
+++ MySQL/Exemplo Script Modelo Escola.sql
@@ -0,0 +1,16 @@
+create database exemplo;
+
+use exemplo;
+
+-- Cadastro de tabela aluno
+create table aluno (
+id_aluno int primary key
+);
+
+create table funcionarios (
+id_funcionarios int primary key
+);
+
+create table cursos (
+id_cursos int primary key 
+);
