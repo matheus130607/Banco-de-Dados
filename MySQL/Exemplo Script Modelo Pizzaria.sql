764dcbe4be393c91772d4c33cd689c8dabf69bcb
matheus130607 <matheusmalaman2014@gmail.com>
2025-08-01 16:42:31 -0300

    Atualizado

---
+++ MySQL/Exemplo Script Modelo Pizzaria.sql
@@ -0,0 +1,15 @@
+create database pizzaria;
+
+use pizzaria;
+
+create table pizza (
+nome_pizza int primary key 
+);
+
+create table alterar (
+alterar_nome_pizza int primary key
+);
+
+create table vender (
+nome_pizza_venda int primary key
+);
